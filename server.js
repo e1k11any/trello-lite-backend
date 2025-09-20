8f9c17152673430524a2bd9263cb4a529efb088d
Ibrahim Yasser <ibrahim7101999@gmail.com>
2025-09-20 07:43:48 +0300

    feat: Configure MongoDB connection using Mongoose

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-// Import the configured app from the src folder
+require("dotenv").config(); // This must be at the very top
+
 const app = require("./src/app");
+const connectDB = require("./src/config/db");
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
