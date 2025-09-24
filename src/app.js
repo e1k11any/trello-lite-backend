7747ea677739fb2cdfef55a06689055cd18d5ab8
Ibrahim Yasser <ibrahim7101999@gmail.com>
2025-09-24 17:08:33 +0300

    Welcome message to refirect users for the aight path

--- src/app.js
+++ src/app.js
@@ -6,4 +6,11 @@ app.use(express.json());
 
 app.use("/api", api);
 
+// Add a root route for a welcome message
+app.get("/", (req, res) => {
+  res.send(
+    "<h1>Welcome to the Trello-Lite API</h1><p>Your API is live and running. Use /api to access the routes.</p>"
+  );
+});
+
 module.exports = app;
